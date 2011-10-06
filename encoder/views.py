2ed538cb223b54eebd3440b7c2c3cee4a6d6e6fd
Alexander Lemann <ablemann@bsu.edu>
2011-10-06 14:51:08 -0400

    Views, templates, URLs so that users can delete their own comments.

--- encoder/views.py
+++ encoder/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import redirect, render_to_response
+from django.shortcuts import redirect, render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 from django.template import RequestContext
+from django.http import HttpResponseForbidden
+from django.core.urlresolvers import reverse
 
 from encoder import models as encoder 
 from encoder import forms as forms
@@ -15,7 +17,7 @@ def get_username(request):
 
 def demo_video(request, identifier):
     v = encoder.Video.objects.get(identifier=identifier)
-    comments = v.media_ptr.commments.all()
+    comments = v.media_ptr.commments.order_by('created_time')
     comment_form = forms.CommentForm()
     if request.POST:
       comment_form = forms.CommentForm(request.POST, request=request)
@@ -40,12 +42,35 @@ def demo_audio(request, identifier):
         add_seek_links(a)
     return render_to_response('encoder/demo/audio.html', {'audio' : a,
                                                           'comments': comments})
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(encoder.Comment, id=comment_id)
+    if comment.commenter != request.user:
+        return HttpResponseForbidden()
+    context = RequestContext(request, {
+        'username': request.user.username,
+        'comment': comment
+    })
+    return render_to_response('encoder/delete_comment.html', context)
+
+def delete_comment_confirm(request, comment_id):
+    comment = get_object_or_404(encoder.Comment, id=comment_id)
+    print 'post?'
+    if request.POST:
+        print 'post!'
+        if comment.commenter != request.user:
+            print 'access denied'
+            return HttpResponseForbidden()
+        print 'delete'
+        comment.delete()
+    return redirect(reverse('home'))
 
 def home(request):
-    media = list(encoder.Video.objects.all()) + list(encoder.Audio.objects.all())
+    videos = encoder.Video.objects.order_by('-encode_end_time')
+    sounds = encoder.Audio.objects.order_by('-encode_end_time')
     comments = encoder.Comment.objects.order_by('-created_time')[:10]
     context = {
-        'media' : media,
+        'videos' : videos,
+        'sounds' : sounds,
         'username': get_username(request),
         'comments' : comments
     }
